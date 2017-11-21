2eaf88ffea5ce9421a0ee53069d61c8af768b631
halstedlarsson <halsted.larsson@gmail.com>
2017-11-21 16:48:37 -0500

    Migrated cards def to separate file.
    Moved cards file out so that example code was more general.

---
+++ cards.js
@@ -0,0 +1,26 @@
+var cards = [
+  { name: "Never Go Alone",
+    icon: "Wand.png",
+    steps: [
+      "+1 Magic Token.",
+      "Trade 2 cards with the player on your left, if both of you are willing and able."
+    ],
+    rarity: 'C',
+  },
+  { name: "Smoke Bomb",
+    icon: "Spy.png",
+    steps: [
+      "+1 Spy Token.",
+      "Remove all cards in play.",
+      "Gain 1 Spy token for each card removed this way.",
+    ],
+    rarity: 'R',
+  },
+  { name: "Triage",
+    icon: "Doctor.png",
+    steps: [
+      "Give all but 1 of your cards to another player.",
+      "+1 Doctor Token for each card in the Patient player's hand."
+    ]
+  },
+];
